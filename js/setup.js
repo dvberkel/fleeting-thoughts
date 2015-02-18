09261ac3445e0cb803e406fd1187e3c0cbf50118
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-18 21:41:53 +0100

    Use query string to influence to evaporation time
    the evaporation time can be set with the '?evaporate=200000' query string

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,22 @@
 (function($, Fleeting){
     $(function(){
-        var view = new Fleeting.CharactersView({ 
-            el : $("body"), 
+	var search = window.location.search.slice(1).split("=");
+	var evaporate = 5000;
+	var index = search.indexOf('evaporate')
+	if (index !== -1 && search[index + 1]) {
+	    evaporate = parseInt(search[index + 1]) || evaporate;
+	}
+
+	console.log(evaporate);
+
+        var view = new Fleeting.CharactersView({
+            el : $("body"),
             strategy : function(){
                 this.hidden(true);
             },
-            evaporateTime : 5000
+            evaporateTime : evaporate
         });
-        
+
         (function loop(){
             window.setTimeout(loop, 1000);
             view.tick();
