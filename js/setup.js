3130da9a00dc678ec9f5768d2ade42921570a1bc
dvberkel <daan.v.berkel.1980@gmail.com>
2015-02-18 21:45:13 +0100

    Please jslint

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,13 @@
 (function($, Fleeting){
     $(function(){
-	var search = window.location.search.slice(1).split("=");
-	var evaporate = 5000;
-	var index = search.indexOf('evaporate')
-	if (index !== -1 && search[index + 1]) {
-	    evaporate = parseInt(search[index + 1]) || evaporate;
-	}
+        var search = window.location.search.slice(1).split("=");
+        var evaporate = 5000;
+        var index = search.indexOf('evaporate');
+        if (index !== -1 && search[index + 1]) {
+            evaporate = parseInt(search[index + 1], 10) || evaporate;
+        }
 
-	console.log(evaporate);
+        console.log(evaporate);
 
         var view = new Fleeting.CharactersView({
             el : $("body"),
